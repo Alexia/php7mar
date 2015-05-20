0c416235c831102ca2c85359474b80943feb5b07
Alexia E. Smith <washuu@gmail.com>
2015-05-19 23:08:08 -0500

    Commit so I can work remotely.

---
+++ classes/tests.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MAR Tests Class
+ * Base class for MAR tests including common regular expressions and base logic.
+ *
+ * @author     Alexia E. Smith <washuu@gmail.com>
+ * @copyright  2015 Alexia E. Smith
+ * @link       https://github.com/Alexia/php7mar
+ */
+
+class tests {
+	$commonRegex = [
+		'variable'	=> '[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*';
+	];
+}
+?>
