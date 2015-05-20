f8a3fac296c215937e40c5c756a56cf705c29f7e
Alexia E. Smith <washuu@gmail.com>
2015-05-20 13:15:10 -0500

    Skeleton scanner class.

---
+++ classes/scanner.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHP 7 MAR
+ * Scanner Class
+ *
+ * @author     Alexia E. Smith <washuu@gmail.com>
+ * @copyright  2015 Alexia E. Smith
+ * @link       https://github.com/Alexia/php7mar
+ */
+
+namespace mar;
+
+class scanner {
+	/**
+	 * Main Constructor
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function __construct() {
+		//@TODO: All this.  Ideally this class will take a file or folder path and return a list of file(s) to be opened and iterated over.
+	}
+}
+?>
